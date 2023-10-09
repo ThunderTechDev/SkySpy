6b4a65768f7d69d6759fbbad27a6c5216f566e62
Sergio González Cristóbal <thundertechdev@icloud.com>
2023-10-09 23:11:33 +0200

    Initial Commit

---
+++ SkySpy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkySpy
+//
+//  Created by Sergio Gonzalez Cristobal on 9/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkySpy/SkySpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkySpyApp.swift
+//  SkySpy
+//
+//  Created by Sergio Gonzalez Cristobal on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SkySpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
