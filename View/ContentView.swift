8b65064cb35d08f92b30221fa3309dca915d9f47
Sergio González Cristóbal <thundertechdev@icloud.com>
2023-10-16 23:37:44 +0200

    - Added app title "SkySpy" with a custom font and centered alignment. - Implemented dynamic background image adaptation for dark mode. - Introduced a TextField for city name input with adaptive background and shadow for both light and dark modes. - Enhanced the design with adaptive shadows and background colors for various UI elements. - Modified the UI element for displaying temperature and city name. - Adjusted alignments and paddings for better centering and spacing of UI components.

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -10,37 +10,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var cityName: String = ""
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
         ZStack {
-            Image("BackgroundSkySpy")
-                .resizable(resizingMode: .stretch)
-                .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
+            Image(colorScheme == .dark ? "BackgroundSkySpyDark" : "BackgroundSkySpy")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fill)
+                            .edgesIgnoringSafeArea(.all)
             
             VStack {
-                Spacer()  // Empuja el área de la imagen hacia abajo
+                // Título de la App
+                Spacer().frame(height: 50)
+                ZStack {
+                    Text("SkySpy")
+                        .font(Font.custom("SF Compact Rounded", size: 64.0))
+                        .blur(radius: 3.0)
+                        .fontDesign(.rounded)
+                        .fontWeight(.bold)
+                    Text("SkySpy")
+                        .font(Font.custom("SF Compact Rounded", size: 64))
+                        .foregroundStyle(.white)
+                        .fontDesign(.rounded)
+                        .fontWeight(.bold)
+                }
+                .shadow(color: Color.black.opacity(0.6), radius: 10, x: 0, y: 10)
+                
+           
+                
+                // TextField para el nombre de la ciudad
+                TextField("Introduce el nombre de la ciudad", text: $cityName)
+                    .padding(10)
+                    .background(Color(UIColor.secondarySystemBackground).opacity(0.7))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 30)
+                    .shadow(color: colorScheme == .dark ? Color.white.opacity(0.1) : Color.black.opacity(0.6),
+                            radius: 10, x: 0, y: 10)
+                
+                Spacer()  // Espacio entre el TextField y el área de la imagen
+                
+                // Campo de texto grande para los grados de temperatura
+                Text("22°C")  // Puedes vincular esto a una variable si es necesario
+                    .font(.system(size: 70))
+                    .fontWeight(.bold)
+                
+                // Campo de texto para indicar el nombre de la ciudad
+                Text("Ciudad")
+                    .font(.title2)
+                 
+                
+                Spacer()  // Espacio entre los campos de texto y el área de la imagen
                 
                 // Área para la imagen en la parte inferior
                 ZStack {
                     RoundedRectangle(cornerRadius: 30)  // Marcos
-                        .fill(Color.white)  // Fondo blanco
+                        .fill(Color(UIColor.systemBackground))  // Fondo dinámico
                         .frame(height: 400)  // Ajusta la altura según lo necesites
-                        
+                        .shadow(color: Color.black.opacity(0.6), radius: 10, x: 0, y: 10)
                     RoundedRectangle(cornerRadius: 30)  // Borde
                         .stroke(lineWidth: 8)
                         .frame(height: 400)  // Ajusta la altura según lo necesites
                         .foregroundColor(.gray)
-                    
-                    /*Image("Pendiente!!")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 200)  // Asegúrate de que la altura coincida con la altura del marco*/
                 }
                 .padding(30)  // Añade un poco de espacio alrededor del área de la imagen
+                
             }
+            
         }
     }
 }
+
+
 
 
 #Preview {
