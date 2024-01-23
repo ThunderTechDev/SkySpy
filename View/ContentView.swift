bbf63a7ffa61cb0e97c1c2020b196ab54d9e14e5
Sergio González Cristóbal <thundertechdev@icloud.com>
2024-01-24 00:31:14 +0100

    Implemented Location Services and Weather Display Features
    - Configured a new structure to access location services, retrieving the current city name where the device is located.
    - Added functionality to display local weather information for the current city upon app launch, enhancing user experience with immediate, location-relevant data.

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -8,6 +8,7 @@
 
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
     
@@ -22,138 +23,142 @@ struct ContentView: View {
     
     
     var body: some View {
-    
-        ZStack {
-            backgroundImage
-            mainContent
-        }
-        .onAppear {
-            print(locationManager.cityName)
-        }
-        .onTapGesture {
-            isFirstResponder = false
-        }.ignoresSafeArea(.keyboard, edges: .bottom)
-    }
-    
-    var mainContent: some View {
-        VStack {
-            appTitle
-            cityTextField
-            temperatureDisplay
-            cityNameDisplay
-            imageArea
-        }
-        .padding(.horizontal, 30)
-    }
-    
-    
-    // MARK: - UI Components
-    
-    var backgroundImage: some View {
-        Image(weatherManager.isDaytime == false ? "darkBackground" : "lightBackground")
-            .resizable(resizingMode: .stretch)
-            .edgesIgnoringSafeArea(.all)
-    }
-    
-    var appTitle: some View {
-        VStack {
-            Spacer().frame(height: 10)
-            ZStack {
-                Text("SkySpy")
-                    .font(Font.custom("SF Compact Rounded", size: 64.0))
-                    .blur(radius: 3.0)
-                    .fontDesign(.rounded)
-                    .fontWeight(.bold)
-                Text("SkySpy")
-                    .font(Font.custom("SF Compact Rounded", size: 64))
-                    .foregroundStyle(.white)
-                    .fontDesign(.rounded)
-                    .fontWeight(.bold)
+           ZStack {
+               backgroundImage
+               mainContent
+           }
+           .onChange(of: locationManager.cityName, {
+               cityName = locationManager.cityName
+               searchCity()
+           })
+           .onTapGesture {
+               isFirstResponder = false
+           }
+           .ignoresSafeArea(.keyboard, edges: .bottom)
+       }
+
+        
+        var mainContent: some View {
+            VStack {
+                appTitle
+                cityTextField
+                temperatureDisplay
+                cityNameDisplay
+                imageArea
             }
-            .shadow(color: Color.black.opacity(0.6), radius: 10, x: 0, y: 10)
+            .padding(.horizontal, 30)
         }
-    }
-    
-    var cityTextField: some View {
-        HStack {
-            TextField(placeHolder, text: $cityName, onCommit: {
-                searchCity()
-            })
-            .autocorrectionDisabled()
-            .padding(10)
-            .background(Color(UIColor.secondarySystemBackground).opacity(0.7))
-            .cornerRadius(10)
-            .focused($isFirstResponder)
-            .shadow(color: weatherManager.isDaytime == false ? Color.white.opacity(0.5) : Color.black.opacity(0.5),
-                    radius: 15)
-            .textInputAutocapitalization(.words)
-            
-            searchButton
-        }
-    }
-    
-    var searchButton: some View {
-        Button(action: {
-            searchCity()
-        }) {
-            Image(systemName: "magnifyingglass")
-                .font(.title)
-                .foregroundColor(weatherManager.isDaytime  ? .black : .white)
-                .shadow(color: weatherManager.isDaytime == false ? .white : Color.black.opacity(0.5),
-                        radius: 6)
-        }
-    }
-    
-    var temperatureDisplay: some View {
-        Text(String(format: "%.1fº", weatherManager.currentWeather?.temperature ?? 0.0))
-            .font(.system(size: 70))
-            .fontWeight(.bold)
-            .foregroundColor(weatherManager.isDaytime  ? .black : .white)
-            
         
-    }
-    
-    var cityNameDisplay: some View {
-        Text(currentCity)
-            .font(.title2)
-            .foregroundColor(weatherManager.isDaytime  ? .black : .white)
-            .shadow(color: weatherManager.isDaytime ? .white : .black,
-                    radius: 4)
-    }
-    
-    var imageArea: some View {
         
-        GeometryReader { geometry in
-            ZStack {
-                if let imageName = weatherManager.currentWeather?.conditionName {
-                    // Imagen disponible
-                    Image(weatherManager.isDaytime ? "\(imageName).png" : "\(imageName)_night.png")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                        .clipShape(RoundedRectangle(cornerRadius: 30))
-                    
-                   
-                    
-                    // Marco con sombra y borde cuando la imagen está presente
-                    RoundedRectangle(cornerRadius: 30)
-                        .stroke(lineWidth: 8)
-                        .foregroundColor(.gray)
-                } else {
-                    // No hay imagen disponible
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color(weatherManager.isDaytime == false ? UIColor.black : UIColor.white))
-                        .shadow(color: Color.black.opacity(0.6), radius: 10, x: 0, y: 10)
-                    
-                    // Marco con sombra y borde cuando no hay imagen
-                    RoundedRectangle(cornerRadius: 30)
-                        .stroke(lineWidth: 8)
-                        .foregroundColor(.gray)
+        // MARK: - UI Components
+        
+        var backgroundImage: some View {
+            Image(weatherManager.isDaytime == false ? "darkBackground" : "lightBackground")
+                .resizable(resizingMode: .stretch)
+                .edgesIgnoringSafeArea(.all)
+        }
+        
+        var appTitle: some View {
+            VStack {
+                Spacer().frame(height: 10)
+                ZStack {
+                    Text("SkySpy")
+                        .font(Font.custom("SF Compact Rounded", size: 64.0))
+                        .blur(radius: 3.0)
+                        .fontDesign(.rounded)
+                        .fontWeight(.bold)
+                    Text("SkySpy")
+                        .font(Font.custom("SF Compact Rounded", size: 64))
+                        .foregroundStyle(.white)
+                        .fontDesign(.rounded)
+                        .fontWeight(.bold)
                 }
+                .shadow(color: Color.black.opacity(0.6), radius: 10, x: 0, y: 10)
             }
-            .frame(width: geometry.size.width, height: geometry.size.height)
         }
-    }
+        
+        var cityTextField: some View {
+            HStack {
+                TextField(placeHolder, text: $cityName, onCommit: {
+                    searchCity()
+                })
+                .autocorrectionDisabled()
+                .padding(10)
+                .background(Color(UIColor.secondarySystemBackground).opacity(0.7))
+                .cornerRadius(10)
+                .focused($isFirstResponder)
+                .shadow(color: weatherManager.isDaytime == false ? Color.white.opacity(0.5) : Color.black.opacity(0.5),
+                        radius: 15)
+                .textInputAutocapitalization(.words)
+                
+                searchButton
+            }
+        }
+        
+        var searchButton: some View {
+            Button(action: {
+                searchCity()
+            }) {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(weatherManager.isDaytime  ? .black : .white)
+                    .shadow(color: weatherManager.isDaytime == false ? .white : Color.black.opacity(0.5),
+                            radius: 6)
+            }
+        }
+        
+        var temperatureDisplay: some View {
+            Text(String(format: "%.1fº", weatherManager.currentWeather?.temperature ?? 0.0))
+                .font(.system(size: 70))
+                .fontWeight(.bold)
+                .foregroundColor(weatherManager.isDaytime  ? .black : .white)
+            
+            
+        }
+        
+        var cityNameDisplay: some View {
+            Text(currentCity)
+                .font(.title2)
+                .foregroundColor(weatherManager.isDaytime  ? .black : .white)
+                .shadow(color: weatherManager.isDaytime ? .white : .black,
+                        radius: 4)
+        }
+        
+        var imageArea: some View {
+            
+            GeometryReader { geometry in
+                ZStack {
+                    if let imageName = weatherManager.currentWeather?.conditionName {
+                        // Imagen disponible
+                        Image(weatherManager.isDaytime ? "\(imageName).png" : "\(imageName)_night.png")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: geometry.size.width, height: geometry.size.height)
+                            .clipShape(RoundedRectangle(cornerRadius: 30))
+                        
+                        
+                        
+                        // Marco con sombra y borde cuando la imagen está presente
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(lineWidth: 8)
+                            .foregroundColor(.gray)
+                    } else {
+                        // No hay imagen disponible
+                        RoundedRectangle(cornerRadius: 30)
+                            .fill(Color(weatherManager.isDaytime == false ? UIColor.black : UIColor.white))
+                            .shadow(color: Color.black.opacity(0.6), radius: 10, x: 0, y: 10)
+                        
+                        // Marco con sombra y borde cuando no hay imagen
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(lineWidth: 8)
+                            .foregroundColor(.gray)
+                    }
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height)
+            }
+        }
+    
+
     
     private func searchCity() {
         Task {
@@ -169,7 +174,11 @@ struct ContentView: View {
             }
         }
     }
+    
+    
 }
+
+
 
 #Preview {
     ContentView()
--- ViewModel/LocationManager.swift
+++ ViewModel/LocationManager.swift
@@ -5,39 +5,45 @@
 //  Created by Sergio Gonzalez Cristobal on 19/1/24.
 //
 
+import Foundation
 import CoreLocation
-import SwiftUI
 
-@Observable class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    var locationData: LocationData?
-    var cityName: String = "Inicializacion inicial"
-    
-    private var locationManager = CLLocationManager()
-    private let geocoder = CLGeocoder()
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    @Published var cityName: String = "Cargando..."
 
     override init() {
         super.init()
         locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization() // Solicita permiso al usuario
+        locationManager.startUpdatingLocation() // Comienza a obtener la ubicación
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        locationData = LocationData(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        lookupCityName(from: location)
-    }
-
-    private func lookupCityName(from location: CLLocation) {
-        geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks, error) in
-            guard let self = self, let placemark = placemarks?.first, error == nil else { return }
-            self.cityName = placemark.locality ?? "Ubicación desconocida"
-
+        if let location = locations.last {
+            updateCityName(from: location)
         }
     }
- 
-}
 
+    private func updateCityName(from location: CLLocation) {
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            if let error = error {
+                print("Error en Geocoding: \(error.localizedDescription)")
+                return
+            }
+
+            if let placemark = placemarks?.first, let city = placemark.locality {
+                DispatchQueue.main.async {
+                    self.cityName = city
+                }
+            }
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error al obtener la ubicación: \(error.localizedDescription)")
+    }
+}
 
 
 
